c6401c6d2aa2e89916a67be8c5d11b3f0842ceb2
Dan Tasse <dan.tasse@gmail.com>
2015-07-24 11:07:18 -0700

    run it in a loop

--- here_traffic_testing.py
+++ here_traffic_testing.py
@@ -12,7 +12,7 @@
 # more about the requests library at: http://docs.python-requests.org/en/latest/
 # 1. install pip (if you don't have it) from here: http://pip.readthedocs.org/en/latest/installing.html
 # 2. install requests with "pip install requests"
-import requests, ConfigParser, pprint
+import requests, ConfigParser, pprint, json, time
 
 config = ConfigParser.ConfigParser()
 config.read('config.txt')
@@ -23,12 +23,33 @@ appCode = config.get('here', 'appCode')
 # >>> r = requests.get("http://httpbin.org/get", params=payload)
 url_params = {'app_id': appId, 'app_code': appCode, 'bbox': '39.8485715,-86.0969867;39.8358934,-86.0757964'}
 url = 'http://traffic.cit.api.here.com/traffic/6.1/flow.json'
-resp = requests.get(url, params=url_params)
 
-resp_json = resp.json()
+output_file = open('traffic.json', 'w')
 
-pprint.pprint(resp_json)
-# print resp_json['CREATED_TIMESTAMP']
+output_file.write('[')
 
+while True:
+    resp = requests.get(url, params=url_params)
+    resp_json = resp.json()
+    # manipulate this json here to get out whatever we want
+    # rws = resp_json['RWS'][0]['RW']
+    # streets = []
+    # for obj in rws:
+    #     streets.append(obj['DE'])
+    # etc.
+
+    # for street in streets:
+    #     output_file.write(street + "\n")
+
+    # Or just dump the raw json.
+
+    # pprint.pprint(resp_json)
+    output_file.write(resp_json + ',\n')
+    # json.load(file)
+    # json.dump(resp_json, output_file)
+    # print resp_json['CREATED_TIMESTAMP']
+    time.sleep(300)
+
+output_file.write(']')
 # what does it all mean:
 # http://traffic.cit.api.here.com/traffic/6.0/xsd/flow.xsd?app_id=DemoAppId01082013GAL%20&app_code=AJKnXv84fjrb0KIHawS0Tg
